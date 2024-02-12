e31af128fdfad2fa810c9889aaebd638a75572a8
Renatuscape <rrjonsberg@gmail.com>
2024-02-12 15:29:39 +0100

    Added controller for collections

---
+++ PagefinderDb/Controllers/CollectionController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PagefinderDb.Data;
+using PagefinderDb.Data.Models;
+
+namespace PagefinderDb.Controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class CollectionController : ControllerBase
+    {
+        private readonly PagefinderDbContext _db;
+
+        public CollectionController(PagefinderDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCollections()
+        {
+            var collections = await _db.Collections
+                .Include(c => c.User)
+                .Include(c => c.Stories)!
+                .ThenInclude(s => s.Pages)
+                .ToListAsync();
+            return Ok(collections);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCollection(int id)
+        {
+            var collection = await _db.Collections
+                .Include(c => c.User)
+                .Include(c => c.Stories)!
+                .ThenInclude(s => s.Pages)!
+                .ThenInclude(p => p.Choices)
+                .FirstOrDefaultAsync(c => c.Id == id);
+            if (collection == null)
+            {
+                return NotFound();
+            }
+            return Ok(collection);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCollection(Collection collection)
+        {
+            await _db.Collections.AddAsync(collection);
+            await _db.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetCollection), new { id = collection.Id }, collection);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateCollection(int id, Collection collection)
+        {
+            if (id != collection.Id)
+            {
+                return BadRequest();
+            }
+
+            _db.Entry(collection).State = EntityState.Modified;
+            try
+            {
+                await _db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                throw;
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCollection(int id)
+        {
+            var collection = await _db.Collections.FindAsync(id);
+            if (collection == null)
+            {
+                return NotFound();
+            }
+            _db.Collections.Remove(collection);
+            await _db.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
