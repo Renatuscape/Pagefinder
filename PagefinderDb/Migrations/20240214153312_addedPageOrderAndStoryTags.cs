3e9f34f6d44e82d6aa153caceda3786d327de31a
Renatuscape <rrjonsberg@gmail.com>
2024-02-14 16:42:50 +0100

    Updated database with tags for stories and numbering for pages.

---
+++ PagefinderDb/Migrations/20240214153312_addedPageOrderAndStoryTags.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PagefinderDb.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedPageOrderAndStoryTags : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Tags",
+                table: "Stories",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Order",
+                table: "Pages",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Tags",
+                table: "Stories");
+
+            migrationBuilder.DropColumn(
+                name: "Order",
+                table: "Pages");
+        }
+    }
+}
