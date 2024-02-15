d9682267c3d450fa14070572de6a4bb14527ccb6
Renatuscape <rrjonsberg@gmail.com>
2024-02-15 18:17:22 +0100

    Removed old migrations to rebuild database with fewer tables.

--- PagefinderDb/Data/PageFinderDbContext.cs
+++ PagefinderDb/Data/PageFinderDbContext.cs
@@ -18,12 +18,6 @@ namespace PagefinderDb.Data
                 .HasForeignKey(s => s.CollectionId)
                 .OnDelete(DeleteBehavior.Cascade);
 
-            modelBuilder.Entity<PlayTest>()
-                .HasOne(pt => pt.Story)
-                .WithOne(s => s.PlayTest)
-                .HasForeignKey<PlayTest>(pt => pt.StoryId)
-                .OnDelete(DeleteBehavior.Restrict); // Prevent cascade delete
-
             modelBuilder.Entity<Choice>()
                 .HasOne(c => c.SuccessPage)
                 .WithMany() // Assuming there's no navigation property from Story to Requirement
@@ -36,37 +30,12 @@ namespace PagefinderDb.Data
                 .HasForeignKey(c => c.FailurePageId) // Specify the foreign key for FailurePage
                 .OnDelete(DeleteBehavior.Restrict);
 
-            modelBuilder.Entity<Requirement>()
-                .HasOne(r => r.Story)
-                .WithMany() // Assuming there's no navigation property from Story to Requirement
-                .HasForeignKey(r => r.StoryId)
-                .OnDelete(DeleteBehavior.Restrict); // Prevent cascade delete
-
-            modelBuilder.Entity<Restriction>()
-                .HasOne(r => r.Story)
-                .WithMany() // Assuming there's no navigation property from Story to Restriction
-                .HasForeignKey(r => r.StoryId)
-                .OnDelete(DeleteBehavior.Restrict); // Prevent cascade delete
-
-            modelBuilder.Entity<Reward>()
-                .HasOne(r => r.Story)
-                .WithMany() // Assuming there's no navigation property from Story to Reward
-                .HasForeignKey(r => r.StoryId)
-                .OnDelete(DeleteBehavior.Restrict); // Prevent cascade delete
-
             base.OnModelCreating(modelBuilder);
         }
 
-        public DbSet<Character> Characters { get; set; } = default!;
         public DbSet<Choice> Choices { get; set; } = default!;
         public DbSet<Collection> Collections { get; set; } = default!;
-        public DbSet<InventoryItem> InventoryItems { get; set; } = default!;
-        public DbSet<Item> Items { get; set; } = default!;
         public DbSet<Page> Pages { get; set; } = default!;
-        public DbSet<PlayTest> PlayTests { get; set; } = default!;
-        public DbSet<Requirement> Requirements { get; set; } = default!;
-        public DbSet<Restriction> Restrictions { get; set; } = default!;
-        public DbSet<Reward> Rewards { get; set; } = default!;
         public DbSet<Story> Stories { get; set; } = default!;
         public DbSet<User> Users { get; set; } = default!;
 
