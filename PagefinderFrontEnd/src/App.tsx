f4e3f682d5018d0be85779886efc0ec79907ab73
Renatuscape <rrjonsberg@gmail.com>
2024-02-23 17:53:33 +0100

    Moved components to separate files.

--- PagefinderFrontEnd/src/App.tsx
+++ PagefinderFrontEnd/src/App.tsx
@@ -1,59 +1,27 @@
 import { useState } from 'react'
 import './App.css'
+import { UserCard } from './components/UserCard'
+import { LibraryCard } from './components/LibraryCard'
+import { EditorCard } from './components/EditorCard'
+import { ReaderCard } from './components/ReaderCard'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  // const [user, setUser] = useState<User | null>(null)
+  const [user, setUser] = useState<User | null>({ id: 4, username: 'TestUsername', email: '', password: '' })
+  const [readStory, setReadStory] = useState<Story | null>(null);
+  
   return (
     <>
       <h1>Pagefinder</h1>
       <div className="card-container">
-      <div className="card" style={{gridArea: 'user'}}>
-        <h2>User</h2>
-        <p>
-          Login form
-        </p>
-        <p>
-          Information about the active user
-        </p>
-        <form style={{display: 'flex', gap: 10}}>
-          <input placeholder='username' type='text'/>
-          <input placeholder='password' type='password'/>
-          <button>Submit</button>          
-        </form>
-      </div>
-      <div className="card" style={{gridArea: 'portfolio'}}>
-        <h2>Portfolio</h2>
-        <button>New Collection</button>
-        <p>
-          The user's collections. Collapsible. Each collection has an edit button and a new story button.<br/>
-          <button>open/close</button>
-        </p>
-      </div>
-      <div className="card" style={{gridArea: 'collectionManager'}}>
-        <h2>Collection Manager</h2>
-        <p>
-          <img src='' style={{height: 100, width: 100}}/><br/>
-          Collection description<br/>
-          <button>Edit</button><button>Delete</button><button>open/close</button>
-        </p>
-      </div>
-      <div className="card" style={{gridArea: 'storyManager'}}>
-        <h2>Story Manager</h2>
-        <p>
-          <img src='' style={{height: 100, width: 100}}/><br/>
-          Story description<br/>
-          <button>Edit</button><button>Delete</button><button>open/close</button>
-        </p>
-      </div>
-      <div className="card" style={{gridArea: 'reader'}}>
-        <h2>Story Preview</h2>
-        <p>
-          <img src='' style={{height: 100, width: 100}}/><br/>
-          Content printed here<br/>
-          <button>Edit</button><button>Delete</button><button>open/close</button>
-        </p>
-      </div>
+        <div className="card" style={{ gridArea: 'banner', display: 'flex', justifyContent: 'center', alignItems: 'center', height: 35, backgroundColor: '#bfe4e6' }}>
+          {user ? 'Welcome back!' : 'Create a user or log in to work on your stories.'}
+        </div>
+        <UserCard user={user} setUser={setUser} />
+        <LibraryCard user={user} />
+
+        <EditorCard user={user} story={null} collection={null}/>
+        <ReaderCard story={readStory} />
       </div>
       <p className="read-the-docs">
         A storycrafting tool by Renatuscape
