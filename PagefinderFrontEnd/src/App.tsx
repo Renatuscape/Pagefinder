84dc300935c10fe06d51d0dffcc860120bfd030d
Renatuscape <rrjonsberg@gmail.com>
2024-02-23 01:21:12 +0100

    Set up basic layout

--- PagefinderFrontEnd/src/App.tsx
+++ PagefinderFrontEnd/src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,25 +6,57 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <h1>Pagefinder</h1>
+      <div className="card-container">
+      <div className="card" style={{gridArea: 'user'}}>
+        <h2>User</h2>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Login form
+        </p>
+        <p>
+          Information about the active user
+        </p>
+        <form style={{display: 'flex', gap: 10}}>
+          <input placeholder='username' type='text'/>
+          <input placeholder='password' type='password'/>
+          <button>Submit</button>          
+        </form>
+      </div>
+      <div className="card" style={{gridArea: 'portfolio'}}>
+        <h2>Portfolio</h2>
+        <button>New Collection</button>
+        <p>
+          The user's collections. Collapsible. Each collection has an edit button and a new story button.<br/>
+          <button>open/close</button>
         </p>
       </div>
+      <div className="card" style={{gridArea: 'collectionManager'}}>
+        <h2>Collection Manager</h2>
+        <p>
+          <img src='' style={{height: 100, width: 100}}/><br/>
+          Collection description<br/>
+          <button>Edit</button><button>Delete</button><button>open/close</button>
+        </p>
+      </div>
+      <div className="card" style={{gridArea: 'storyManager'}}>
+        <h2>Story Manager</h2>
+        <p>
+          <img src='' style={{height: 100, width: 100}}/><br/>
+          Story description<br/>
+          <button>Edit</button><button>Delete</button><button>open/close</button>
+        </p>
+      </div>
+      <div className="card" style={{gridArea: 'reader'}}>
+        <h2>Story Preview</h2>
+        <p>
+          <img src='' style={{height: 100, width: 100}}/><br/>
+          Content printed here<br/>
+          <button>Edit</button><button>Delete</button><button>open/close</button>
+        </p>
+      </div>
+      </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        A storycrafting tool by Renatuscape
       </p>
     </>
   )
