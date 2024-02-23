89eb068eb8540afc3325545cb554868b78162633
Renatuscape <rrjonsberg@gmail.com>
2024-02-23 17:56:46 +0100

    Created UserCard, which allows the user to edit their user or log in/log out.

---
+++ PagefinderFrontEnd/src/components/UserCard.tsx
@@ -0,0 +1,29 @@
+import { Dispatch, SetStateAction } from "react";
+
+type UserCardProps = {
+  user: User | null;
+  setUser: Dispatch<SetStateAction<User | null>>;
+}
+
+export function UserCard(props: UserCardProps) {
+  return <>
+    <div className="card" style={{ gridArea: 'user' }}>
+      <h2>{props.user ? props.user.username : 'Log In'}</h2>
+
+      {props.user ?
+        <>
+          <div className="button-container">
+            <button>Edit</button>
+            <button onClick={() => props.setUser(null)}>Log Out</button>
+          </div>
+        </>
+        :
+        <form style={{ marginTop: 15, display: 'flex', flexDirection: 'column', gap: 10 }}>
+          <input placeholder='username' type='text' />
+          <input placeholder='password' type='password' />
+          <button>Submit</button>
+          <button>New User</button>
+        </form>}
+    </div>
+  </>
+}
