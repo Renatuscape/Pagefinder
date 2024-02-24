521c7e56cb4afbe0fd3c81fa7acc61b34a4002e7
Renatuscape <rrjonsberg@gmail.com>
2024-02-24 23:42:46 +0100

    Editor now has create story option that can be hidden and revealed. Should allow toggling between create page and create story.

--- PagefinderFrontEnd/src/components/EditorCard.tsx
+++ PagefinderFrontEnd/src/components/EditorCard.tsx
@@ -1,18 +1,43 @@
+import { useState } from "react";
+import { CreateStory } from "./CreateStory";
+
 type EditorCardProps = {
-    user : User | null;
-    story : Story | null;
-    collection : Collection | null;
+    user: User | null;
+    story: Story | null;
+    collection: Collection | null;
 }
 
-export function EditorCard(props: EditorCardProps){
+export function EditorCard(props: EditorCardProps) {
+    const [createStory, setCreateStory] = useState<Boolean>(false);
+    const [createPage, setCreatePage] = useState<Boolean>(false);
 
+    const [story, setStory] = useState<Story | null>(null);
 
-return <>
-    <div className="card" style={{ gridArea: 'editor' }}>
-    <h2>Editor</h2>
-    <div className="button-container">
-    <button>New Story</button><button>New Collection</button><button>Save Changes</button><button>Cancel</button>
-      </div>
-  </div>
-  </>
+    const clickCreateStory = ()=> {
+        setCreateStory(true);
+        setCreatePage(false);
+    }
+
+    const clickCancel = () => {
+        setCreateStory(false);
+        setCreatePage(false);
+    }
+    return <>
+        <div className="card" style={{ gridArea: 'editor', backgroundColor: '#bfe4e6' }}>
+            {props.user ?
+                <>
+                    <h2>Editor</h2>
+                    {createStory && <>
+                        <CreateStory setCreateStory={() => setCreatePage} userID={props.user?.id} />
+                    </>}
+                </>
+                :
+                <div className="card"><h2>Log in to begin creating!</h2></div>
+            }
+
+            <div className="button-container">
+                <button onClick={clickCreateStory}>Create Story</button><button>Create Page</button><button onClick={clickCancel}>Cancel</button>
+            </div>
+        </div>
+    </>
 }
