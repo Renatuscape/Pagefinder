3f8aaffed216baf5c85cc4632846270625e2402e
Renatuscape <rrjonsberg@gmail.com>
2024-02-26 12:45:10 +0100

    Added delete function to collection cards.

--- PagefinderFrontEnd/src/components/CollectionCard.tsx
+++ PagefinderFrontEnd/src/components/CollectionCard.tsx
@@ -5,9 +5,29 @@ type CollectionCardProps = {
     collection: Collection;
 }
 
+async function deleteCollectionAsync(id: number): Promise<void> {
+
+    const res = await fetch(`https://localhost:7177/collection/${id}`,
+    { method: 'DELETE' });
+
+    if (!res.ok) {
+        console.log(res);
+        throw new Error('could not delete user from backend');
+    }
+}
+
 export function CollectionCard(props: CollectionCardProps) {
-    const [isCollapsed, setIsCollapsed] = useState<boolean>(false);
+    const [isCollapsed, setIsCollapsed] = useState<boolean>(true);
     const collection = props.collection;
+
+    const handleDelete = async () => {
+        try {
+            await deleteCollectionAsync(props.collection.id!);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     return <>
         <div className="collection-thumbnail">
             <div style={{ borderRadius: 0, width: '100%', height: 150, backgroundColor: '#66a2ad', backgroundImage: `url(${collection.imageUrl ? collection.imageUrl : '/public/images/icon_collection_default.png'})` }}>
@@ -25,7 +45,7 @@ export function CollectionCard(props: CollectionCardProps) {
                 </>}
 
                 <div className="button-container">
-                    <button>Edit</button>
+                    <button onClick={handleDelete}>Delete</button>
                     <button onClick={()=> setIsCollapsed(!isCollapsed)}>{isCollapsed ? 'Expand' : 'Collapse'}</button>
                     <button>New Story</button>
                 </div>
