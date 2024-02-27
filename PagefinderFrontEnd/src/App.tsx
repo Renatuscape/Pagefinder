730b2653b04b79161407656dbe7fe266f1df137d
Renatuscape <rrjonsberg@gmail.com>
2024-02-27 12:50:54 +0100

    Changed initial user fetch to get the whole library of collections and stories, so that child components will not need to run their own fetch requests.

--- PagefinderFrontEnd/src/App.tsx
+++ PagefinderFrontEnd/src/App.tsx
@@ -6,7 +6,7 @@ import { EditorCard } from './components/EditorCard'
 import { ReaderCard } from './components/ReaderCard'
 
 async function fetchUserAsync(id: number | undefined): Promise<User> {
-  const response = await fetch(`https://localhost:7177/User/${id}`);
+  const response = await fetch(`https://localhost:7177/User/${id}/library`);
 
   try {
       const resObject = await response.json();
@@ -23,12 +23,25 @@ async function fetchUserAsync(id: number | undefined): Promise<User> {
 
 function App() {
   // const [user, setUser] = useState<User | null>(null)
-  const [user, setUser] = useState<User | null>({ id: -1, username: 'Default', email: '', password: '' })
+  const [user, setUser] = useState<User | null>({ id: -1, username: 'Default', email: '', password: '', collections: [] })
   const [readStory, setReadStory] = useState<Story | null>(null);
   
   useEffect(() => {
     fetchUserAsync(4).then(user => setUser(user));
 }, [])
+
+    // Function to update the user object with the edited collection
+    const updateCollectionInUser = (updatedCollection: Collection) => {
+      setUser(prevUser => {
+          if (prevUser) {
+              const updatedCollections = prevUser.collections?.map(collection =>
+                  collection.id === updatedCollection.id ? updatedCollection : collection
+              );
+              return { ...prevUser, collections: updatedCollections };
+          }
+          return prevUser;
+      });
+  };
 
   return (
     <>
@@ -40,7 +53,7 @@ function App() {
         <UserCard user={user} setUser={setUser} />
         <LibraryCard user={user} />
 
-        <EditorCard user={user} story={null} collection={null}/>
+        <EditorCard user={user} story={null} collection={null} updateCollectionInUser={updateCollectionInUser}/>
         <ReaderCard story={readStory} />
       </div>
       <p className="read-the-docs">
